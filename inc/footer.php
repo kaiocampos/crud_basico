90d10a6087a3f15968034639ddcdb78d32b39286
Kaio Campos <kaiocamposti@gmail.com>
2020-02-24 18:18:38 -0300

    Centralizando o Copy

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
     </main> <!-- /container -->	
     <hr>
-    <footer class="container">			<p>&copy;2019 - Kaio Campos</p>		</footer>	
+    <footer class="container text-center" >			<p>&copy;2020 - Kaio Campos</p>		</footer>	
         <script src="<?php echo BASEURL; ?>js/jquery.min.js"></script>	    
         <script>window.jQuery || document.write('<script src="<?php echo BASEURL; ?>js/jquery.min.js"><\/script>')</script>	
         <script src="<?php echo BASEURL; ?>js/bootstrap.min.js"></script>	
